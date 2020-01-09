d2c21caca5e13c8130b7eb8830851000d1d62bb2
shawinmihail <shawinmihail@gmail.com>
2020-01-09 19:59:06 +0300

    sensors

--- plot/plot_state.m
+++ plot/plot_state.m
@@ -11,14 +11,19 @@ theta = out.state.Data(:,3);
 
 figure
 hold on;
-plot(x, 'LineWidth', lw, 'Color', 'r');
-plot(y, 'LineWidth', lw, 'Color', 'b');
-title('Абсолютная ошибка скорости', 'FontSize', fs);
-plot_legend(1).str(:) = 'Измерения';
-plot_legend(2).str(:) = 'Оценка';
-lgd = legend(plot_legend(:).str);
-lgd.FontSize = fs;
-set(gca,'FontSize',fs)
-xlabel('Время, сек')
-ylabel('м')
-% xlim([0 220])
+plot(x, y, 'LineWidth', lw, 'Color', 'r');
+% plot(y, 'LineWidth', lw, 'Color', 'b');
+title('Траектория', 'FontSize', fs);
+% plot_legend(1).str(:) = 'Измерения';
+% plot_legend(2).str(:) = 'Оценка';
+% lgd = legend(plot_legend(:).str);
+% lgd.FontSize = fs;
+% set(gca,'FontSize',fs)
+% xlabel('Время, сек')
+% ylabel('м')
+% % xlim([0 220])
+
+figure
+hold on;
+plot(t, theta, 'LineWidth', lw, 'Color', 'r');
+title('Руль', 'FontSize', fs);
--- simulink/init.m
+++ simulink/init.m
@@ -2,5 +2,26 @@ close all
 clc
 clear
 
-%%
+%% sim
+seed = 200;
+rng(seed);
+dt = 1e-2;
+
+%% env
+g = [0 0 -10];
+
+%% state
 initial_state_local = [0;0;0];
+
+%% ctrl
+initial_ctrl = [0;0];
+
+%% noise
+% gps
+gps_pos_local_rsm = 1.66;
+gps_vel_local_rsm = 0.05;
+
+% imu
+imu_acc_rsm = 0.25;
+imu_rot_vel_rsm = 0.05;
+
